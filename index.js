ea37972d807c603b04531dfdac7f3515fa48f0e3
backspacev9 <backspacev9@gmail.com>
2022-06-27 02:33:23 +0300

    feat: add validation for number input

--- index.js
+++ index.js
@@ -8,41 +8,71 @@ class Base {
 }
 
 const numberInput = document.getElementById("numberInput");
-const finputNumber = document.getElementById("finput-number");
+const holderInput = document.getElementById("holderInput");
+const monthInput = document.getElementById("monthInput");
+const yearInput = document.getElementById("yearInput");
+
+const formInputNumber = document.getElementById("formInput-number");
+const formInputName = document.getElementById("formInput-name");
 const numbersEl = [];
+const holderEl = [];
+const monthEl = [];
+const yearEl = [];
 var prev = 0;
 
-for (let i = 0; i < 16; i++) {
-  numbersEl.push(new Base("div", ["char", "numberChar"], "#"));
-}
+const initCharElements = () => {
+  for (let i = 0; i < 16; i++) {
+    numbersEl.push(new Base("div", ["char", "numberChar"], "#"));
+  }
+  for (let i = 0; i < 2; i++) {
+    monthEl.push(new Base("div", ["char", "monthChar"], "M"));
+  }
+  for (let i = 0; i < 2; i++) {
+    yearEl.push(new Base("div", ["char", "yearChar"], "Y"));
+  }
 
-numbersEl.forEach((el) => {
-  numberInput.append(el.element);
-});
+  numbersEl.forEach((el) => {
+    numberInput.append(el.element);
+  });
+  monthEl.forEach((el) => {
+    monthInput.append(el.element);
+  });
+  yearEl.forEach((el) => {
+    yearInput.append(el.element);
+  });
+};
 
-finputNumber.addEventListener("input", (ev) => {
+formInputNumber.addEventListener("input", (ev) => {
+  if (/^-?\d*$/.test(ev.target.value) === false) {
+    formInputNumber.value = formInputNumber.value.slice(0, -(ev.target.value.length - prev));
+    return;
+  }
+  let cursorPos = ev.target.selectionStart;
   let numbers = ev.currentTarget.value.split("");
-  let i = numbers.length > 0 ? numbers.length - 1 : numbers.length;
+  let k = cursorPos - 1;
   if (prev > numbers.length) {
-    i = prev - 1;
-    moveDown(numbersEl[i].element);
-  } else [moveUp(numbersEl[i].element)];
-
-  numbersEl[i].element.innerText = numbers[i] ? numbers[i] : "#";
-
+    //erase
+    k = numbers.length;
+    animate(numbersEl[k].element, "move_up");
+  } else {
+    //inputing
+    animate(numbersEl[k].element, "move_down");
+    animate(numbersEl[prev].element, "move_down");
+  }
+  for (let i = 0; i < numbersEl.length; i++) {
+    numbersEl[i].element.innerText = numbers[i] ? numbers[i] : "#";
+  }
   prev = numbers.length;
-  console.log(numbers);
+});
+formInputName.addEventListener("input", (evt) => {
+  formInputName.value = formInputName.value.replace(/[^0-9.]/g, "").replace(/(\..*?)\..*/g, "$1");
 });
 
-const moveUp = (element) => {
-  element.classList.add("moveUp");
+const animate = (element, animation = "") => {
+  element.style.animation = `0.2s linear 0s alternate ${animation}`;
   element.addEventListener("animationend", (ev) => {
-    element.classList.remove("moveUp");
+    element.removeAttribute("style");
   });
 };
-const moveDown = (element) => {
-  element.classList.add("moveDown");
-  element.addEventListener("animationend", (ev) => {
-    element.classList.remove("moveDown");
-  });
-};
+
+initCharElements();
