c282c03a7855151f12b0069c581810c47493d597
backspacev9 <backspacev9@gmail.com>
2022-06-28 02:13:00 +0300

    feat: complete card holder input and correct animations

--- index.js
+++ index.js
@@ -9,16 +9,19 @@ class Base {
 
 const numberInput = document.getElementById("numberInput");
 const holderInput = document.getElementById("holderInput");
-const monthInput = document.getElementById("monthInput");
-const yearInput = document.getElementById("yearInput");
+const monthSelect = document.getElementById("monthSelect");
+const yearSelect = document.getElementById("yearSelect");
 
 const formInputNumber = document.getElementById("formInput-number");
 const formInputName = document.getElementById("formInput-name");
+
+const currentYear = new Date().getFullYear();
 const numbersEl = [];
-const holderEl = [];
+let holderEl = [];
 const monthEl = [];
 const yearEl = [];
-var prev = 0;
+let prevNumbers = 0;
+let prevHolders = 0;
 
 const initCharElements = () => {
   for (let i = 0; i < 16; i++) {
@@ -40,32 +43,60 @@ const initCharElements = () => {
   yearEl.forEach((el) => {
     yearInput.append(el.element);
   });
+
+  for (i = 1; i <= 12; i++) {
+    let month = i.toLocaleString("en-US", {
+      minimumIntegerDigits: 2,
+      useGrouping: false,
+    });
+    let option = new Base("option", [], month);
+    option.element.setAttribute("value", month);
+    monthSelect.append(option.element);
+  }
+  for (i = 2015; i <= currentYear; i++) {
+    let option = new Base("option", [], i);
+    option.element.setAttribute("value", i);
+    yearSelect.append(option.element);
+  }
 };
 
 formInputNumber.addEventListener("input", (ev) => {
   if (/^-?\d*$/.test(ev.target.value) === false) {
-    formInputNumber.value = formInputNumber.value.slice(0, -(ev.target.value.length - prev));
+    formInputNumber.value = formInputNumber.value.slice(0, -(ev.target.value.length - prevNumbers));
     return;
   }
   let cursorPos = ev.target.selectionStart;
   let numbers = ev.currentTarget.value.split("");
   let k = cursorPos - 1;
-  if (prev > numbers.length) {
+  if (prevNumbers > numbers.length) {
     //erase
     k = numbers.length;
     animate(numbersEl[k].element, "move_up");
   } else {
     //inputing
     animate(numbersEl[k].element, "move_down");
-    animate(numbersEl[prev].element, "move_down");
+    animate(numbersEl[prevNumbers].element, "move_down");
   }
   for (let i = 0; i < numbersEl.length; i++) {
     numbersEl[i].element.innerText = numbers[i] ? numbers[i] : "#";
   }
-  prev = numbers.length;
+  prevNumbers = numbers.length;
+  //formInputName.value = formInputName.value.replace(/[^0-9.]/g, "").replace(/(\..*?)\..*/g, "$1");
 });
-formInputName.addEventListener("input", (evt) => {
-  formInputName.value = formInputName.value.replace(/[^0-9.]/g, "").replace(/(\..*?)\..*/g, "$1");
+formInputName.addEventListener("input", (ev) => {
+  if (/^[a-zA-Z ]+$/.test(ev.target.value) === false && ev.target.value) {
+    formInputName.value = formInputName.value.slice(0, -(ev.target.value.length - prevHolders));
+    return;
+  }
+  let cursorPos = ev.target.selectionStart;
+  let chars = ev.target.value.split("");
+  holderInput.innerHTML = "";
+  holderEl = chars.map((el) => new Base("div", ["char", "holderChar"], el).element);
+  if (prevHolders < chars.length) animate(holderEl[cursorPos - 1], "tilt_rigth");
+  holderEl.forEach((el) => {
+    holderInput.append(el);
+  });
+  prevHolders = chars.length;
 });
 
 const animate = (element, animation = "") => {
