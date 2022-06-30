6474d910caed9ab9ae38e6f3b971539060b164b0
backspacev9 <backspacev9@gmail.com>
2022-06-30 03:02:41 +0300

    fix: correct inputs logic

--- index.js
+++ index.js
@@ -9,15 +9,18 @@ class Base {
 
 const numberInput = document.getElementById("numberInput");
 const holderInput = document.getElementById("holderInput");
-const monthSelect = document.getElementById("monthSelect");
-const yearSelect = document.getElementById("yearSelect");
 const cwInput = document.getElementById("cwInput");
 
 const card = document.getElementById("card");
+const floatBorder = document.getElementById("floatBorder");
+const cardHolder = document.getElementById("cardHolder");
+const expires = document.getElementById("expires");
 
 const formInputNumber = document.getElementById("formInput-number");
 const formInputName = document.getElementById("formInput-name");
 const FormCwInput = document.getElementById("form-cwInput");
+const monthSelect = document.getElementById("monthSelect");
+const yearSelect = document.getElementById("yearSelect");
 
 const currentYear = new Date().getFullYear();
 const EXAMPLE_HOLDER = "Example holder card".split("");
@@ -60,25 +63,40 @@ const initCharElements = () => {
 };
 
 formInputNumber.addEventListener("input", (ev) => {
-  if (/^-?\d*$/.test(ev.target.value) === false) {
+  // ^[\d ]*$
+  // ^-?\d*$
+  if (/^[\d]*$/.test(ev.target.value) === false) {
     formInputNumber.value = formInputNumber.value.slice(0, -(ev.target.value.length - prevNumbers));
     return;
   }
   let cursorPos = ev.target.selectionStart;
-  let numbers = ev.currentTarget.value.split("");
+  //let fullValue = ev.currentTarget.value.split("");
+  let numbers = ev.currentTarget.value.replaceAll(" ", "").split("");
+
   let k = cursorPos - 1;
+  //console.log(fullValue[cursorPos - 1]);
+  // if (fullValue[cursorPos - 1] != " ") {
   if (prevNumbers > numbers.length) {
     //erase
     k = numbers.length;
     animate(numbersEl[k].element, "move_up");
   } else {
     //inputing
-    animate(numbersEl[k].element, "move_down");
+    //console.log(cursorPos - 1 + "-and-" + prevNumbers);
+    if (cursorPos - 1 == prevNumbers) {
+      animate(numbersEl[k].element, "move_down");
+    }
     animate(numbersEl[prevNumbers].element, "move_down");
   }
+  // }
+
   for (let i = 0; i < numbersEl.length; i++) {
     numbersEl[i].element.innerText = numbers[i] ? numbers[i] : "#";
   }
+  // const regexedInput = ev.currentTarget.value.replaceAll(" ", "").replace(/.{4}/g, "$& ");
+  // formInputNumber.value =
+  //   formInputNumber.value.length !== 19 ? regexedInput : regexedInput.slice(0, -1);
+
   prevNumbers = numbers.length;
   //formInputName.value = formInputName.value.replace(/[^0-9.]/g, "").replace(/(\..*?)\..*/g, "$1");
 });
@@ -112,7 +130,7 @@ yearSelect.addEventListener("input", (ev) => {
 
 FormCwInput.addEventListener("input", (ev) => {
   if (/^-?\d*$/.test(ev.target.value) === false) {
-    FormCwInput.value = formInputNumber.value.slice(0, -(ev.target.value.length - prevCW));
+    FormCwInput.value = FormCwInput.value.slice(0, -(ev.target.value.length - prevCW));
     return;
   }
 
@@ -127,11 +145,44 @@ FormCwInput.addEventListener("blur", () => {
   card.classList.toggle("flip");
 });
 
+formInputNumber.addEventListener("focus", () => {
+  moveFloatBorder(numberInput, formInputNumber);
+});
+formInputName.addEventListener("focus", () => {
+  moveFloatBorder(cardHolder, formInputName);
+});
+monthSelect.addEventListener("focus", () => {
+  moveFloatBorder(expires, monthSelect);
+});
+yearSelect.addEventListener("focus", () => {
+  moveFloatBorder(expires, yearSelect);
+});
+
+FormCwInput.addEventListener("focus", () => {
+  hideFloatBorder();
+});
+
 const animate = (element, animation = "") => {
   element.style.animation = `0.2s linear 0s alternate ${animation}`;
   element.addEventListener("animationend", (ev) => {
     element.removeAttribute("style");
   });
 };
-
+const moveFloatBorder = (moveToEl, focusedEl) => {
+  floatBorder.style.opacity = 1;
+  floatBorder.style.width = moveToEl.offsetWidth + "px";
+  floatBorder.style.height = moveToEl.offsetHeight + "px";
+  floatBorder.style.left = moveToEl.offsetLeft + "px";
+  floatBorder.style.top = moveToEl.offsetTop + "px";
+  focusedEl.addEventListener("blur", (ev) => {
+    hideFloatBorder();
+  });
+};
+const hideFloatBorder = () => {
+  floatBorder.style.left = 0;
+  floatBorder.style.top = 0;
+  floatBorder.style.width = 100 + "%";
+  floatBorder.style.height = 100 + "%";
+  floatBorder.style.opacity = 0;
+};
 initCharElements();
